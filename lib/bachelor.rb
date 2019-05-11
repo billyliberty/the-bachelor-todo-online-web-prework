3d73aa886c3a3ba97db7bdf3ad3443409229252b
Joshua Crum <joshua.crum@gmail.com>
2019-05-11 18:27:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,9 +18,15 @@ def get_contestant_name(data, occupation)
   end
 end
   
-
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |describe|
+      if describe["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
